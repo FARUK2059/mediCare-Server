088f4c664d1902c7e58bdc56849871c874418b29
FARUK2059 <omarfaruk34552@gmail.com>
2024-06-04 02:06:33 +0600

    medicin data get successfully

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -45,6 +45,48 @@ async function run() {
 
         // Database Conection
         const userCollections = client.db("madiCare").collection("users");
+        const medicinCollections = client.db("madiCare").collection("medicin");
+
+
+        // ***************  Veryfy secure related API  ********************
+
+        // jwt Token send to client side
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        // middlewares 
+
+        // Veryfy Token
+        const verifyToken = (req, res, next) => {
+            console.log('verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+
+        }
+
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollections.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
 
         //  ***************  user funtionality **************
 
@@ -61,6 +103,16 @@ async function run() {
             const result = await userCollections.insertOne(user);
             res.send(result);
         })
+
+        // ****************  Medicin data function ********************
+
+        // get all medicin data
+        // get menu data from mongodeb database
+        app.get('/medicin', async (req, res) => {
+            const result = await medicinCollections.find().toArray();
+            res.send(result);
+        });
+
 
 
 
